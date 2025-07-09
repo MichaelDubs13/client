31f1eb3e46569bcd03982e39acdaaec37f547639
Michael Ballinger <miballinger@tesla.com>
2025-07-08 21:41:48 -0400

    MCP ETH Port identification update

--- src/components/ui/eecPage/Components/MCPs/EthPortConfiguration.jsx
+++ src/components/ui/eecPage/Components/MCPs/EthPortConfiguration.jsx
@@ -17,6 +17,7 @@ const EthPortConfiguration = ({mcp, index}) => {
         <div>
             <h7>Port 1 (Ring topology - In)</h7>
             <DropdownItem title={"Target location (e.g., ++LINE+MCP02-XPF-ETH01:P2)"} item={mcp} index={mcpIndex} property={"eth_port1_target_location"} options={portOptions}/>
+            <h7>Port 2 (Ring topology - Out)</h7>
             <DropdownItem title={"Target location (e.g., ++LINE+MCP02-XPF-ETH01:P1)"} item={mcp} index={mcpIndex} property={"eth_port2_target_location"} options={portOptions}/>
         </div>  
     );
